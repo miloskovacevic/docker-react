68051e7fc1113d8241f7af4d392a5a929a01ffc6
Miloš Kovačević <miloscpt@gmail.com>
2019-01-10 22:16:32 +0100

    initial commit

---
+++ build/precache-manifest.a4e9185dd1240c684d995309a54ac5fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "398257c29971763ee519",
+    "url": "/static/js/main.398257c2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "398257c29971763ee519",
+    "url": "/static/css/main.9507e19a.chunk.css"
+  },
+  {
+    "revision": "d028c50608ab62b47cd7fe3d52045bc7",
+    "url": "/index.html"
+  }
+];
